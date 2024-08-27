ae457dd9e32b5e74fab3ddffe4702f5de1c64b2e
APerson13 <65836435+APerson13@users.noreply.github.com>
2024-08-26 18:58:05 -0700

    Fix missing assets directory
    install.sh: Make an empty assets directory under ~/.config/caveripper to work around CaveRipper requiring that directory to exist to do anything.

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ set -e
 echo "Copying resources folder into $HOME/.config/caveripper/"
 mkdir -p ~/.config/caveripper
 cp -r resources ~/.config/caveripper
+# CaveRipper requires $HOME/.config/caveripper/assets to exist as a directory, even if it is empty.
+mkdir ~/.config/caveripper/assets
 
 echo "Installing Caveripper"
 cargo install --path ./cli
